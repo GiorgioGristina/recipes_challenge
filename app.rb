221a5bb58759971e1a8d683231509157019df2e6
giorgio <giorgio.gristina91@gmail.com>
2022-10-19 11:39:55 +0100

    put logic into app.rb file to list all the recipes in the terminal

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 # file: app.rb
 
 require_relative 'lib/database_connection'
+require_relative 'lib/recipe_repository'
 
 # We need to give the database name to the method `connect`.
 DatabaseConnection.connect('recipes')
+
+recipes = RecipeRepository.new
+
+recipes.all.each do |recipe|
+    puts "- #{recipe.name} cooking time: #{recipe.cooking_time} rating: #{'⭐️' * recipe.rating}"
+end
